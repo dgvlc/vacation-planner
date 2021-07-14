c763d835827e692ae8299e8b4c63298007c36f37
diego <dgvlc@yahoo.com>
2021-07-14 15:00:37 -0400

    need to add a homepage somehow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from "react-router-dom"
 import VacationPlace from "./components/VacationPlace"
 import './App.css';
 import Navbar from "./components/Navbar";
+import VacationPlaceDetail from "./components/VacationPlaceDetail";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Route>
       <Route>
         <VacationPlace />
+      </Route>
+      <Route path="/vacations/:id">
+        <VacationPlaceDetail />
       </Route>
     </div>
   );
--- src/components/VacationPlaceDetail.jsx
+++ src/components/VacationPlaceDetail.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import { BASE_URL, headers } from "../services"
+import axios from "axios"
 
 export default function VacationPlaceDetail() {
+  const [vacation, setVacation] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchVacation = async () => {
+
+  const URL = `${BASE_URL}/${id}`
+  const respose = await axios.get(URL, {
+        headers,
+      })
+      setVacation(respose.data)
+    }
+    fetchVacation()
+  }, [id])
+  
   return (
     <div>
-      
+      {vacation.fields?.name}
     </div>
   )
 }
