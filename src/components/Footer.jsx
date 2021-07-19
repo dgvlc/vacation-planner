b2b4b16833d2987c7127172eca78a8d7e9096076
diego <dgvlc@yahoo.com>
2021-07-19 16:14:12 -0400

    complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import "./Footer.css"
 // npm install --save-dev @iconify/react @iconify-icons/logos
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import githubIcon from '@iconify-icons/logos/github-icon'         
-import { Link } from 'react-router-dom';
 
 
 
@@ -11,9 +10,9 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <div className="footer-box">
-    <Link to="https://github.com/dgvlc">
-        <Icon icon={githubIcon} />
-    </Link>
+    <a  className="social-link" href="https://github.com/dgvlc">
+        <Icon className="icon" icon={githubIcon} />
+    </a>
     </div>
   )
 }
